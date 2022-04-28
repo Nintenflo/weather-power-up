bd90a43b8c5eac42c0e50044a4dac006ef4227de
Flogo Lito <nintenflo@gmail.com>
2022-04-28 10:59:20 +0200

    adding fetch weather api

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,22 @@ console.log('Hello World!');
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         //return an array of card badges for the given card
-        return [];
+        return t.card('all')
+            .then(function (card) {
+                console.log(card);
+                fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.610769&lon=3.876716&appid=2ff5f0db9251c46e0020f6a608efdeaf')
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (weatherData) {
+                        return [{
+                            text: weatherData.main.temp.toString()
+                        },
+                        {
+                            text: weatherData.wind.speed.toString()
+                        }]
+                    });
+
+            });
     }
-})
+});
