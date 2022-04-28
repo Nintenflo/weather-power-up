620715210ab1544ce74b1d6192f79d74fb4e6dcd
Flogo Lito <nintenflo@gmail.com>
2022-04-28 18:53:40 +0200

    change api call

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,20 +5,25 @@ window.TrelloPowerUp.initialize({
         //return an array of card badges for the given card
         return t.card('all')
             .then(function (card) {
-                console.log(card);
-                return fetch('https://api.openweathermap.org/data/2.5/weather?lat=43.610769&lon=3.876716&appid=%%APP_ID%%')
+                // console.log(card);
+                return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=43.610769&lon=3.876716&exclude=minutely,hourly,alerts&appid=%%APP_ID%%')
                     .then(function (response) {
                         console.log(response);
                         return response.json();
                     })
                     .then(function (weatherData) {
-                        console.log("weatherdata: ");
-                        console.log(weatherData);
+                        // console.log("weatherdata: ");
+                        // console.log(weatherData);
+                        console.log(card);
+
                         return [{
-                            text: weatherData.main.temp.toString()
+                            icon: `https://openweathermap.org/img/w/${weatherData.daily[0].weather.icon}.png`,
+                            text: weatherData.daily[0].weather.main
                         },
                         {
-                            text: weatherData.wind.speed.toString()
+                            icon: `https://openweathermap.org/img/w/${weatherData.daily[1].weather.icon}.png`,
+                            text: weatherData.daily[1].weather.main
+
                         }]
                     });
 
